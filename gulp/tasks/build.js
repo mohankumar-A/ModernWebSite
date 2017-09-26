054f22e82a57cae29fa0ab25f3a04f6675b4cca5
Mohan Kumar <sweetperson.mk@gmail.com>
2017-09-26 19:52:38 +0530

    Completed gulp build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,6 +1,64 @@
 var gulp = require("gulp");
+var imageMin = require("gulp-imagemin");
+var del = require("del");
+var usemin = require("gulp-usemin");
+var rev = require("gulp-rev");
+var cssnano = require("gulp-cssnano");
+var uglify = require("gulp-uglify");
+var browserSync = require("browser-sync");
 
 
-gulp.task("build", [], function () {
-    
+gulp.task("previewDist", function(){
+   browserSync.init({
+       notify: false,
+       server: {
+           baseDir: "docs"
+       }
+   });
+});
+
+gulp.task("deleteDistFolder", ["icons"], function () {
+   return del("./docs");
+});
+
+gulp.task("copyGenFiles", ["deleteDistFolder"], function () {
+
+    var pathToCopy = [
+                        "./app/**/*",
+                        "!./app/index.html",
+                        "!./app/assets/images/**",
+                        "!./app/assets/styles/**",
+                        "!./app/assets/scripts/**",
+                        "!./app/assets/temp",
+                        "!./app/assets/temp/**",
+                    ];
+   return gulp.src(pathToCopy)
+       .pipe(gulp.dest("./docs"));
+});
+
+gulp.task("optimizeImages", ["deleteDistFolder"], function () {
+   return gulp.src(["./app/assets/images/**/*", "!./app/assets/images/icons", "!./app/assets/images/icons/**/*"])
+       .pipe(imageMin({
+           progressive: true,
+           interlaced: true,
+           multipass: true
+       }))
+       .pipe(gulp.dest("./docs/assets/images"))
+});
+
+gulp.task("useMinTrigger", ["deleteDistFolder"], function () {
+   gulp.start("useMin");
+});
+
+gulp.task("useMin", ["styles", "scripts"], function () {
+   return gulp.src("./app/index.html")
+       .pipe(usemin({
+           css: [function() {return rev()}, function() {return cssnano()}],
+           js: [function () {return rev()}, function() {return uglify()}]
+       }))
+       .pipe(gulp.dest("./docs"))
+});
+
+gulp.task("build", ["deleteDistFolder", "copyGenFiles", "optimizeImages", "useMinTrigger"], function () {
+
 });
